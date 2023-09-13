36d742a6c67102b053834a62c281500091d537bd
Artur <144278265+Artursaraujo@users.noreply.github.com>
2023-09-13 16:56:26 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#1
+
+numero = 2
+numero2 = 3
+print(numero + numero2)
+
+comprimento = input ("Seu nome-")
+print(comprimento) 
+
+comprimento2 = input ("Qual o seu nome completo")
+print(comprimento2)
+
+numero1 = 5
+numero5 = 5
+print( numero1 * numero5)
